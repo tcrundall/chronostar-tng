8045234b23f195dd50877513828bc9346ac043f9
Timothy Crundall <tim.crundall@gmail.com>
2022-10-27 12:51:00 +0200

    Close #39: Tidy up dumping of results:int,final

--- src/chronostar/driver.py
+++ src/chronostar/driver.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import inspect
+from io import TextIOWrapper
 from pathlib import Path
 from typing import Any, Callable, Type, Union
 import yaml
@@ -18,6 +20,14 @@ from .component.spherespacetimecomponent import SphereSpaceTimeComponent
 from .mixture.componentmixture import ComponentMixture
 from .introducer.simpleintroducer import SimpleIntroducer
 from .icpool.simpleicpool import SimpleICPool
+
+
+def heading_str(heading, sym='-', top=True):
+    line = sym * len(heading)
+    if top:
+        return f'\n{line}\n{heading}\n{line}\n'
+    else:
+        return f'\n{heading}\n{line}\n'
 
 
 class Driver:
@@ -144,72 +154,11 @@ class Driver:
             icpool.register_result(label, m, -m.bic(data))
 
             if self.intermediate_dumps:
-                self.dump_mixture_result(label, m, data)
+                dump_dir = self.savedir_path / label
+                self.dump_mixture_result(dump_dir, label, m, data)
 
         # loop will end when icpool stops generating initial conditions
         return icpool.best_mixture
-
-    def dump_mixture_result(
-        self,
-        label: str,
-        mixture: BaseMixture,
-        data: NDArray[float64],
-    ) -> None:
-        """Store the result of a mixture to file
-
-        Parameters
-        ----------
-        label : str
-            The unique label of the initial condition of the mixture
-        mixture : BaseMixture
-            The (fitted) mixture model
-        data : NDArray of shape (n_samples, n_features)
-            The input data
-        """
-        # Make directory
-        mixture_dir = self.savedir_path / label
-        mixture_dir.mkdir(parents=True, exist_ok=True)
-
-        # Save model parameters in numpy arrays
-        weights, components = mixture.get_parameters()
-        memberships = mixture.estimate_membership_prob(data)
-        np.save(str(mixture_dir / 'weights.npy'), weights)
-        np.save(str(mixture_dir / 'memberships.npy'), memberships)
-        for i, comp in enumerate(components):
-            np.save(str(mixture_dir / f"comp_{i:03}_params.npy"), comp.get_parameters())
-
-        # Write information to file for quick overview
-        results_file = mixture_dir / f'{label}.txt'
-        with open(results_file, 'w') as fp:
-            fp.write(f"Results of {label}\n")
-            fp.write("----------------------------------------------\n")
-            fp.write(f"BIC: {mixture.bic(data)}\n")
-            fp.write(f"weights: {weights}\n")
-            fp.write(f"total members: {memberships.sum(axis=0)}\n")
-            fp.write("\n")
-            fp.write("--------------------\n")
-            fp.write("Component parameters\n")
-            fp.write("--------------------\n")
-            for i, comp in enumerate(components):
-                fp.write(f"Component {i:03}:\n")
-                fp.write("^^^^^^^^^^^^^^^^^\n")
-                fp.write(f"{comp.get_parameters()}\n")
-                fp.write("\n")
-
-            fp.write("\n")
-            fp.write("----------------------\n")
-            fp.write("Configuration settings\n")
-            fp.write("----------------------\n")
-            yaml.dump(self.config_params, fp)
-
-            fp.write("\n")
-            fp.write("------------\n")
-            fp.write("Classes used\n")
-            fp.write("------------\n")
-            fp.write(f"{self.component_class=}\n")
-            fp.write(f"{self.mixture_class=}\n")
-            fp.write(f"{self.introducer_class=}\n")
-            fp.write(f"{self.icpool_class=}\n")
 
     def read_config_file(
         self,
@@ -255,3 +204,98 @@ class Driver:
                 raise UserWarning(f"[CONFIG] {key} not recognised!")
 
         return config_params
+
+    def dump_mixture_result(
+        self,
+        dump_dir,
+        label: str,
+        mixture: BaseMixture,
+        data: NDArray[float64],
+    ) -> None:
+        """Store the result of a mixture to file
+
+        Parameters
+        ----------
+        label : str
+            The unique label of the initial condition of the mixture
+        mixture : BaseMixture
+            The (fitted) mixture model
+        data : NDArray of shape (n_samples, n_features)
+            The input data
+        """
+        # Make directory
+        dump_dir.mkdir(parents=True, exist_ok=True)
+
+        # Save model parameters in numpy arrays
+        weights, components = mixture.get_parameters()
+        memberships = mixture.estimate_membership_prob(data)
+        np.save(str(dump_dir / 'weights.npy'), weights)
+        np.save(str(dump_dir / 'memberships.npy'), memberships)
+        for i, comp in enumerate(components):
+            np.save(str(dump_dir / f"comp_{i:03}_params.npy"), comp.get_parameters())
+
+        # Write information to file for quick overview
+        results_file = dump_dir / f'{label}.txt'
+        with open(results_file, 'w') as fp:
+            fp.write(f"Results of {label}\n")
+            fp.write("----------------------------------------------\n")
+            fp.write(f"BIC: {mixture.bic(data)}\n")
+            fp.write(f"weights: {weights}\n")
+            fp.write(f"total members: {memberships.sum(axis=0)}\n")
+            fp.write(heading_str("Component parameters"))
+            for i, comp in enumerate(components):
+                fp.write(heading_str(f"Component {i:03}:", sym='^', top=False))
+                fp.write(f"{comp.get_parameters()}\n")
+                fp.write("\n")
+
+            fp.write(heading_str("Configuration settings"))
+            self.dump_all_config_params(fp)
+
+    def dump_all_config_params(self, fp: TextIOWrapper):
+        """Get all configurable parameters and write them to file
+
+        This method attempts to identify all class level parameters that
+        could be configurable and writes them all to file.
+
+        Parameters
+        ----------
+        fp : TextIOWrapper
+            File pointer to output file
+        """
+
+        def get_simple_attributes(target_class):
+            # Get everything in class which isn't a routine
+            attributes = inspect.getmembers(
+                target_class, lambda x: not inspect.isroutine(x)
+            )
+
+            # Filter out built in attributes
+            attributes = [
+                a for a in attributes
+                if not (a[0].startswith('__') and a[0].endswith('__'))
+            ]
+
+            # Filter for booleans, string, floats, integers and classes
+            attributes = [
+                a for a in attributes
+                if isinstance(a[1], (bool, str, float, int)) or inspect.isclass(a[1])
+            ]
+
+            return attributes
+
+        classes = {
+            "driver": self,
+            "component": self.component_class,
+            "mixture": self.mixture_class,
+            "introducer": self.introducer_class,
+            "icpool": self.icpool_class,
+        }
+        for cname, cl in classes.items():
+            fp.write(f"{cname}:\n")
+            for name, value in get_simple_attributes(cl):
+                if isinstance(value, str):
+                    fp.write(f'    {name}: "{value}"\n')
+                elif isinstance(value, float):
+                    fp.write(f'    {name}: {value:.1e}\n')
+                else:
+                    fp.write(f'    {name}: {value}\n')
